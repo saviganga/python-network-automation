66f37db58c2d722182d9fb860339088bfe3f8289
saviganga <asavioura@gmail.com>
2021-01-03 02:05:22 +0100

    reading from config files with yaml

--- vlanconfig.py
+++ vlanconfig.py
@@ -1,39 +1,35 @@
 ''' Write a script that connects to a device(s) (cisco switch), logs in and sets up vlan configurations '''
 
-
-
-
 import time
+import yaml
+from create_vlans import create_vlan
 
-
-
-
-def save_config(devicee):
+def send_save_config(devicee):
 
     '''sends configuration commands to save running configuration to startup configuration'''
+        
+    with open('yaml-configs/save_config.yaml') as save_file:
+        save_commands = yaml.full_load(save_file)
 
-    save_commands = ['exit', 'copy run start', 'y']
     print('-'*50)
     print(f'\nsaving configurations for {devicee}...')
     time.sleep(1)
-
     for command in save_commands:
-
         print(f'\ncommand sent...{command}')
         time.sleep(1)
     print('-'*50)
 
 
-
-
-def vlan_config(devicee):
+def send_vlan_config(devicee):
 
     '''sends configuration commands to set up valns'''
+
+    with open('yaml-configs/vlan_commands.yaml') as vlan_config_file:
+        vlan_commands = yaml.full_load(vlan_config_file)
 
     print('-'*50)
     print(f'\nsending vlan configuration commands for {devicee}...\n')
     time.sleep(1)
-
     for command in vlan_commands:
         v_id = command.get('id')
         v_name = command.get('name')
@@ -46,44 +42,34 @@ def vlan_config(devicee):
     print('-'*50)
 
 
-
-
-def login_config(devicee):
+def send_login_config(devicee):
 
     '''sends configuration commands to log into a device'''
 
-    login_commands = ['enable', 'conf t',]
+    with open('yaml-configs/login_config.yaml') as login_file:
+        login_commands = yaml.full_load(login_file)
+
     print('-'*50)
     print(f'\nsending login configuration commands for {devicee}...')
     time.sleep(1)
-
     for command in login_commands:
         print(f'\ncommand sent...{command}')
         time.sleep(1)
-    print('-'*50)
-        
-
+    print('-'*50)     
 
 
 def get_vlan_command():
 
     '''gets vlan configuration commands from user and returns a list of the commands'''
 
-    vlans_list = []
-
-    #get the number of vlans from the user
-    num_vlans = int(input('Enter the number of vlans to create: '))
-
-    for c in range(1, num_vlans+1):
-        vlans = {}
-        print(f'\nVLAN setup {c}\n')
-        vlan_id = input('vlan id: ')
-        vlan_name = input('vlan name: ')
-        vlans['id'] = vlan_id
-        vlans['name'] = vlan_name
-        vlans_list.append(vlans)
-    return vlans_list
-
+    new_vlan_list = ['y', 'n']
+    new_vlan = input('add new vlan?(y/n): ')
+    if new_vlan.lower() not in new_vlan_list:
+        get_vlan_command()
+    elif new_vlan.lower() == 'n':
+        pass
+    elif new_vlan.lower() == 'y':
+        create_vlan()
 
 
 
@@ -96,10 +82,8 @@ def select_switches(number):
     for a in range(1, number+1):
         selected_switch = int(input(f'Enter selected switch {a} id: '))
         print('\n')
-        selected_switches.append(selected_switch)
-    
+        selected_switches.append(selected_switch)    
     return selected_switches
-
 
 
 
@@ -114,15 +98,12 @@ def user_num():
 
 
 
-
 def connect():
 
     '''connects to the devices (switches) selected by the user'''
 
     numb_switches = user_num()
-
     while numb_switches > max_num or numb_switches < 1:
-
         #humans right (sigh)... still capture errors
         if numb_switches > max_num:
             print(f'ERROR: Number of available switches less than {numb_switches}')
@@ -130,34 +111,27 @@ def connect():
         elif numb_switches < 1:
             print(f'ERROR: {numb_switches} is an invalid choice')
             numb_switches = user_num()
-
     #select the switches
     select_switch = select_switches(numb_switches)
-
     #confirm user choice and connect
     try:
-
         connected_switches = []
         for select in select_switch:
             connected_switch = switches[select-1]
             connected_switches.append(connected_switch)
-
     except Exception:
-
         print(f'\nInvalid Input\n')
         connect()
-        
     return connected_switches
-
 
 
 
 if __name__ == "__main__":
 
     #get a list of switches
-    switches = ['ground_floor', 'floor1', 'floor2', 'floor3', 'floor4', 'floor5', 'floor6']
+    with open('yaml-configs/switches.yaml') as switch_file:
+        switches = yaml.full_load(switch_file)
     max_num = len(switches)
-
     #display available switches to user
     print('\nSELECT AN ID TO CONNECT TO A SWITCH\n'.center(15, ' '))
     print(f'SWITCHES\n'.center(15, ' '))
@@ -166,31 +140,25 @@ if __name__ == "__main__":
     for idd, switch in enumerate(switches,start=1):
         print( (str(idd) + '.').ljust(5, ' ') + switch.ljust(5, ' ') + '\n' )
         accepted_input.append(idd)
-
     #connect to the device(s)
     connected_devices = connect()    
-
     #get the vlan commands
-    vlan_commands = get_vlan_command()
-
+    get_vlan_command()
     start_time = time.time()
-
+    
     #configure the device
     for device in connected_devices:
         print(f'\nConnecting to switch {device}...\n')
         time.sleep(1)
-        
         print('#'*50)
         print(f'Connected: {device}\n')
         time.sleep(1)
-        login_config(device)
-        vlan_config(device)
-        save_config(device)
+        send_login_config(device)
+        send_vlan_config(device)
+        send_save_config(device)
         print('#'*50)
     
     stop_time = time.time()
-
     prog_time = round(stop_time - start_time, 2)
-    program_time = int(prog_time/60)
-
-    print(f'\nconfiguration time: {program_time}mins\n')
+    program_time = f'{prog_time}'
+    print(f'\nconfiguration time: {program_time} secs\n')
